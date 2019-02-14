f9551e731b0d69b550ae91129164e0434a59c93d
travisbare <travisbare@gmail.com>
2019-02-14 16:58:07 -0600

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,89 @@
+$(document).ready(function () {
+
+  var targetNum = 0;
+  var blueGem = 0;
+  var greenGem = 0;
+  var purpleGem = 0;
+  var redGem = 0;
+  var wins = 0;
+  var losses = 0;
+  var score = 0;
+
+  function randomNum() {
+    var randomNum = Math.floor(Math.random() * 12) + 1;
+
+    return randomNum
+
+  }
 
 
-$("#random-button").on("click", function () {
-    var empty="";
-     for(i=0; i<9; i++){
-     var randomNum = Math.floor((Math.random() * 9)+1);
-     empty=randomNum + empty;
-   }
+  function initialize() {
+   
+    score = 0;
+    blueGem = randomNum();
+    greenGem = randomNum();
+    purpleGem = randomNum();
+    redGem = randomNum();
+    targetNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
-     
+    $(".targetNum").text(targetNum);
+    $(".totalScore").text(score);
 
-     $("#random-number").prepend("<br><hr>", empty);
+    // console.log(blueGem);
+    // console.log(greenGem);
+    // console.log(purpleGem);
+    // console.log(redGem);
+  }
+
+  initialize();
+
+  $("#blueGem").on("click", function () {
+    // alert("blue gem was clicked", blueGem);
+    console.log("blue = ",blueGem);
+    score += blueGem;
+    console.log("score = ",score);
+    
   });
-   // ...
 
- 
+  $("#greenGem").on("click", function () {
+    // alert("green gem was clicked",greenGem);
+    console.log("green = ", greenGem);
+    score += greenGem;
+    console.log("score = ",score);
+  });
+
+  $("#purpleGem").on("click", function () {
+    // alert("green gem was clicked", purpleGem);
+    console.log("purple = ",purpleGem);
+    score += purpleGem;
+  });
+
+  $("#redGem").on("click", function () {
+    // alert("red gem was clicked",redGem);
+    console.log("red = ",redGem);
+    score += redGem;
+  });
+
+// update and logic 
+
+  $(".gem").on("click", function () {
+    $(".totalScore").text(score);
+
+    if (score > targetNum){
+
+      alert("You lose.");
+      losses+=1;
+      $(".losses").text(losses);
+      initialize();
+
+    }
+
+    if (score == targetNum){
+      alert("You win!");
+      wins += 1;
+      $(".wins").text(wins);
+      initialize();
+    }
+
+  });
+});
